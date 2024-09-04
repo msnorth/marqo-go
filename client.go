d866301f0045b6f915b0a02981147a973a4de91e
Madeleine North <madeleine.s.north@gmail.com>
2024-09-04 12:08:35 -0700

    Add auth support for MarqoCloud
    Allow user to provide an optional API key on client creation.
    This key is passed as a header on every Marqo api call.
    Docs (see MarqoCloud section):
    
    https://docs.marqo.ai/latest/reference/api/search/search/#response

--- client.go
+++ client.go
@@ -22,41 +22,22 @@ func WithLogger(logger *slog.Logger) func(*Client) {
 	}
 }
 
+// WithMarqoCloudAuth sets the API key for authentication if you are using MarqoCloud
+func WithMarqoCloudAuth(apiKey string) func(*Client) {
+	return func(c *Client) {
+		c.apiKey = apiKey
+	}
+}
+
 // Client is the client for the Marqo server
 type Client struct {
 	url       string
 	logger    *slog.Logger
 	reqClient *req.Client
+	apiKey    string // Field to hold the API key for use with MarqoCloud
 }
 
 // NewClient creates a new client for the Marqo server.
-//
-// This method initializes a new client with the specified endpoint URL and optional parameters.
-//
-// Parameters:
-//
-//	url (string): The endpoint URL of your Marqo instance.
-//	opt (...Options): Optional parameters for the client.
-//
-// Returns:
-//
-//	*Client: A new Marqo client instance.
-//	error: An error if the operation fails, otherwise nil.
-//
-// The function performs the following steps:
-// 1. Validates the url parameter.
-// 2. Initializes a new Client instance.
-// 3. Applies the optional parameters to the client.
-// 4. Sets the reqClient if not already set.
-// 5. Returns the new client instance if the operation is successful, otherwise returns an error.
-//
-// Example usage:
-//
-//	client, err := marqo.NewClient("http://localhost:8882")
-//	if err != nil {
-//	    log.Fatalf("Failed to create client: %v", err)
-//	}
-//	fmt.Printf("Client: %+v\n", client)
 func NewClient(url string, opt ...Options) (*Client, error) {
 	if url == "" {
 		return nil, fmt.Errorf("url cannot be empty")
@@ -74,6 +55,11 @@ func NewClient(url string, opt ...Options) (*Client, error) {
 	if client.reqClient == nil {
 		client.reqClient = req.NewClient()
 		client.reqClient.BaseURL = url
+
+		// Add the API key header if provided
+		if client.apiKey != "" {
+			client.reqClient.SetCommonHeader("x-api-key", client.apiKey)
+		}
 	}
 
 	// set default logger
